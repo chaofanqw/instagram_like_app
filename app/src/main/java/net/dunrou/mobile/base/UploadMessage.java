25160af1a401d955e25eeb796cf6c07634c80501
Stephen <chaofanqw@gmail.com>
2018-09-19 21:22:05 +1000

    version v1.4 - 3: complete upload function - v2

--- app/src/main/java/net/dunrou/mobile/base/UploadMessage.java
+++ app/src/main/java/net/dunrou/mobile/base/UploadMessage.java
@@ -1,10 +1,34 @@
 package net.dunrou.mobile.base;
 
+import net.dunrou.mobile.network.UploadImage;
+
 /**
  * Created by Stephen on 2018/9/19.
  */
 
 public class UploadMessage {
     private boolean success;
-    private int number;
+    private String path;
+
+    public UploadMessage(boolean success, String path){
+        this.success = success;
+        this.path = path;
+    }
+
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
